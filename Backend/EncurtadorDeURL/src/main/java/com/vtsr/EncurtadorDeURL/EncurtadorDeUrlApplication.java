27ca11d5c8c5917ce4b0bdca60e583a298576158
Vinicius_Muliterno <viniciusmuliternoti@hotmail.com>
2025-12-08 23:46:24 -0300

    Criação base do projeto
    inicio do projeto arquivo inicial.

---
+++ Backend/EncurtadorDeURL/src/main/java/com/vtsr/EncurtadorDeURL/EncurtadorDeUrlApplication.java
@@ -0,0 +1,13 @@
+package com.vtsr.EncurtadorDeURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorDeUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorDeUrlApplication.class, args);
+	}
+
+}
---
+++ Backend/EncurtadorDeURL/src/test/java/com/vtsr/EncurtadorDeURL/EncurtadorDeUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtsr.EncurtadorDeURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorDeUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
